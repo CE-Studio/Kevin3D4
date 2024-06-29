a75533dc68a1e4c2aea8b20a6acae8d539f802b3
Gav <gtownw77@gmail.com>
2024-06-28 20:50:01 -0500

    Dive and Controls+

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -2,19 +2,25 @@ extends CharacterBody3D
 class_name Player
 
 const SPEED = 10.0
+
 const JUMP_VELOCITY = 15
 const DIVE_VELOCITY = 10
 var jumps = 0
 var dives = 0
 var direction2 = Vector3.ZERO
 var isDiving:bool = false
-
+var Sprint = 1
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 
 func _physics_process(delta):
 	
+	#sprinting
+	if Input.is_action_pressed("Sprint"):
+		Sprint = 2
+	else: 
+		Sprint = 1
 	
 	
 	# Add the gravity.
@@ -34,18 +40,11 @@ func _physics_process(delta):
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if isDiving:
-		if direction:
-			velocity.x = direction.x * SPEED
-			velocity.z = direction.z * SPEED
-			direction2 = direction
-		else:
-			velocity.x = move_toward(velocity.x, 0, SPEED)
-			velocity.z = move_toward(velocity.z, 0, SPEED)
 	
-	elif direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	
+	if direction:
+		velocity.x = direction.x * SPEED * Sprint
+		velocity.z = direction.z * SPEED * Sprint
 		direction2 = direction
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
@@ -61,17 +60,21 @@ func _physics_process(delta):
 		isDiving = true
 	if isDiving:
 		
-		velocity.x = direction2.x * DIVE_VELOCITY
-		velocity.z = direction2.z * DIVE_VELOCITY 
+		velocity.x = direction2.x * DIVE_VELOCITY * Sprint
+		velocity.z = direction2.z * DIVE_VELOCITY * Sprint
 		if Input.is_action_just_pressed("ui_accept"):
 			velocity.x = 0
 			velocity.z = 0
 			isDiving = false
 	
-	
+	if position.y < -100:
+		position = Vector3.ZERO
+		velocity = Vector3.ZERO
 		
 
 	move_and_slide()
+	
+	
 
 
 
@@ -81,6 +84,8 @@ func _process(_delta):
 	else:
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	print(position.y)
+	
+	
 func _input(event):	
 	if Input.is_action_pressed("Camera") and (event is InputEventMouseMotion):
 		rotate_y(event.relative.x/-180)
