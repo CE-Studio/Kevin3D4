a7192a0e8294f98338a9e13dd915fee77979bc66
Gav <gtownw77@gmail.com>
2024-06-28 19:48:41 -0500

    player movement

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -3,9 +3,11 @@ extends CharacterBody3D
 
 const SPEED = 10.0
 const JUMP_VELOCITY = 15
-const DIVE_VELOCITY = 70.0
+const DIVE_VELOCITY = 10
 var jumps = 0
 var dives = 0
+var direction2 = Vector3.ZERO
+var isDiving:bool = false
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -14,12 +16,7 @@ var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 func _physics_process(delta):
 	
 	
-	#dive
-	if Input.is_action_just_pressed("Dive") and dives < 1:
-		velocity.y = DIVE_VELOCITY / 10
-		velocity.x = DIVE_VELOCITY
-		dives += 1
-		$MeshInstance3D.rotate_x(-1)
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -27,6 +24,7 @@ func _physics_process(delta):
 		jumps = 0
 		dives = 0
 		$MeshInstance3D.rotation_degrees = Vector3(0, 0, 0)
+		isDiving = false
 
 	# Handle jump and double jump
 	if Input.is_action_just_pressed("ui_accept") and jumps < 2:
@@ -39,25 +37,29 @@ func _physics_process(delta):
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
+		direction2 = direction
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 	
 	
+	
 	#dive
 	if Input.is_action_just_pressed("Dive") and dives < 1:
-		velocity.y = DIVE_VELOCITY / 20
-		if direction:
-			velocity.x += direction.x * DIVE_VELOCITY
-			velocity.z += direction.z * DIVE_VELOCITY 
+		velocity.y = DIVE_VELOCITY 
 		dives += 1
 		$MeshInstance3D.rotate_x(-1)
+		isDiving = true
+	if isDiving:
+		velocity.x += direction2.x * DIVE_VELOCITY
+		velocity.z += direction2.z * DIVE_VELOCITY 
+		
 
 	move_and_slide()
 
 
 
-func _process(delta):
+func _process(_delta):
 	if Input.is_action_pressed("Camera"):
 		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	else:
