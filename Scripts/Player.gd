0b4fe8d72bfa256c2a38a7bb559e176b9b37d0ff
Gav <gtownw77@gmail.com>
2024-06-28 20:12:19 -0500

    Update Player.gd

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -35,7 +35,14 @@ func _physics_process(delta):
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if isDiving:
-		pass
+		if direction:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+			direction2 = direction
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
+	
 	elif direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
