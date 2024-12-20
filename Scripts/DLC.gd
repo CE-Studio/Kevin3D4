94ac5ebaeefce41ce0ca4b6a7d27a18f599ff68f
clarence112 <clarencepacelli@gmail.com>
2024-12-20 01:22:19 -0500

    dlc unlocked

--- Scripts/DLC.gd
+++ Scripts/DLC.gd
@@ -1,4 +1,11 @@
+extends Button
 class_name DLC
 
 
 static var useOldTextures := false
+static var active := false
+
+
+func _on_toggled(toggled_on: bool) -> void:
+	active = toggled_on
+	$CheckBox.set_pressed_no_signal(active)
